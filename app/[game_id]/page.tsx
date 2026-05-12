bf26302c53e5df450ec2bf3c17452494a0737e5b
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-12 14:42:53 +0200

    add: random grid support

--- app/[game_id]/page.tsx
+++ app/[game_id]/page.tsx
@@ -35,12 +35,16 @@ export default async function Page(props: {
     return <div>
         <header className={"pb-6 " + mono.className}>
             <h3 className="text-xl font-monospace">
-                {isToday(new Date(gameData.createdAt)) ?
-                    "grille du jour" : "archive du"}
+                {params.game_id.startsWith("random_") ?
+                    "grille aléatoire" :
+                    isToday(new Date(gameData.createdAt)) ?
+                        "grille du jour" : "archive du"}
             </h3>
-            <h4 className="text-xl font-monospace">
-                {new Intl.DateTimeFormat('fr-FR').format(gameData.createdAt)}
-            </h4>
+            {!params.game_id.startsWith("random_") &&
+                <h4 className="text-xl font-monospace">
+                    { new Intl.DateTimeFormat('fr-FR').format(gameData.createdAt)}
+                </h4>
+            }
         </header>
         <DokuGrid gameData={gameData as UserFacingGameData} stations={stations} />
     </div>
