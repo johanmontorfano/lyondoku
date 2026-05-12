596563bc61b39fb9620224aec049f28d0705f8fc
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-12 05:09:43 +0200

    add: bold mono

--- scripts/fonts.ts
+++ scripts/fonts.ts
@@ -1,4 +1,4 @@
 import { Space_Grotesk, Space_Mono } from "next/font/google";
 
 export const grotesk = Space_Grotesk();
-export const mono = Space_Mono({ weight: "400" });
+export const mono = Space_Mono({ weight: ["400", "700"] });
