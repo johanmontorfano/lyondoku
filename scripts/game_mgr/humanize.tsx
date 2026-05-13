8d0ac21b9338a4be6c2014100a5abfc27c6558d7
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-13 14:01:36 +0200

    add: new humanization data

--- scripts/game_mgr/humanize.tsx
+++ scripts/game_mgr/humanize.tsx
@@ -71,8 +71,9 @@ export function humanizeConstraint(
         case "stationBorough":
             return `Dans un arrondissement ${op === "odd" ? "impair" : "pair"}`;
         case "stationLocation":
+            if (op === "not") return `Ne se situe pas à ${val}`;
             if (op === "equals") return `Se situe à ${val}`;
-            if (op === "either") return `Se situe à ${formatOr(val)}`;
+            if (op === "includes") return `Se situe à ${formatOr(val)}`;
             break;
         case "connections":
             if (!(val in labels) && isLineName(val))
