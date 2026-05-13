949596233a338f86fa66d11fdfda5167048da99d
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-13 15:33:51 +0200

    fix: reset button is less proeminent, add: better state visibility

--- components/reset_progress.tsx
+++ components/reset_progress.tsx
@@ -1,8 +1,21 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export function ResetProgress() {
-    return <button className="btn btn-primary" onClick={() => {
+    const [disabled, setDisabled] = useState(!localStorage.length);
+
+    useEffect(() => {
+        // we periodically check if local storage is still empty
+        const interval = setInterval(() => {
+            setDisabled(!localStorage.length);
+        }, 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return <button className="btn" disabled={disabled} onClick={() => {
         localStorage.clear();
+        window.location.reload();
     }}>
         Remettre la progession à zéro
     </button>
