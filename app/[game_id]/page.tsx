ac864bfdc31bff3d47426427c3653ef3879e04d1
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-14 09:34:28 +0200

    add: support for 200 grids random batch

--- app/[game_id]/page.tsx
+++ app/[game_id]/page.tsx
@@ -9,7 +9,7 @@ export default async function Page(props: {
     // NOTE: when the game_id is "random", we select one of the 10 random grids
     // generated
     if (params.game_id === "random")
-        params.game_id = `random_${Math.ceil(Math.random() * 10)}`;
+        params.game_id = `random_${Math.floor(Math.random() * 200)}`;
 
     return <SSRDokuLoader id={params.game_id} onNotFound={notFound} />
 }
