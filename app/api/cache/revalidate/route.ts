22a297a2f062570e62f57424f967119927956ba1
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-16 11:42:22 +0200

    fix: now uses deterministic revalidation through cron job

---
+++ app/api/cache/revalidate/route.ts
@@ -0,0 +1,11 @@
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    const auth = req.headers.get("authorization");
+
+    if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
+        revalidatePath("/");
+        return NextResponse.json({ success: false }, { status: 401 });
+    } else return NextResponse.json({ success: true });
+}
