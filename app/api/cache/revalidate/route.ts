b5b1064ce4d2bc7b19bc7babeba25be0aae34ce5
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-17 08:38:59 +0200

    fix: logic issue

--- app/api/cache/revalidate/route.ts
+++ app/api/cache/revalidate/route.ts
@@ -4,8 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
     const auth = req.headers.get("authorization");
 
-    if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
+    if (auth === `Bearer ${process.env.CRON_SECRET}`) {
         revalidatePath("/");
-        return NextResponse.json({ success: false }, { status: 401 });
-    } else return NextResponse.json({ success: true });
+        return NextResponse.json({ success: true });
+    } else return NextResponse.json({ success: false }, { status: 401 });
 }
