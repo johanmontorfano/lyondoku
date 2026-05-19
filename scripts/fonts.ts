475afd83086f7917a9e9044b678c75e66718e4d1
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-19 14:51:13 +0200

    add: poppins

--- scripts/fonts.ts
+++ scripts/fonts.ts
@@ -1,14 +1,16 @@
-import { Space_Grotesk, Space_Mono } from "next/font/google";
+import { Poppins, Space_Grotesk, Space_Mono } from "next/font/google";
 
 // NOTE: the migration towards variable-based fonts inclusion is for the only
 // aim of allowing Ju to test fonts with a easy-to-use UI
 
 export const grotesk = Space_Grotesk({ 
-  subsets: ["latin"],
-  variable: "--font-grotesk" 
+    variable: "--font-grotesk" 
 });
 export const mono = Space_Mono({ 
-  weight: ["400", "700"], 
-  subsets: ["latin"],
-  variable: "--font-mono" 
+    weight: ["400", "700"], 
+    variable: "--font-mono" 
 });
+export const serif = Poppins({
+    weight: ["400", "800"],
+    variable: "--font-serif"
+})
