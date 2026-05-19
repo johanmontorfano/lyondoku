b6c42c5dcec8c9e116374c1bc18a820363353b30
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-19 14:52:39 +0200

    add: footer, add: redesigned navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,42 @@
-import { BsSend } from "react-icons/bs";
+import Image from "next/image";
 import Link from "next/link";
+import Icon from "@/public/icon.svg";
 
 export function Navbar() {
-    return <div className="navbar px-0 w-full">
-        <div className="navbar-start">
-            <Link href="/">
-                <h1 className="font-(family-name:--font-mono) text-5xl font-bold hover:underline">
-                    lyondoku
-                </h1>
+    return <div className="w-full flex flex-col items-center">
+        <Link href="/">
+            <h1 className="text-3xl">
+                LYONDLE
+            </h1>
+        </Link>
+        <div className="mx-auto flex gap-6 mt-3">
+            <Link className="link link-hover" href="/archive">
+                Archive
             </Link>
-        </div>
-        <div className="navbar-end gap-3 items-center">
-            <Link href="mailto:hello@johanmontorfano.com">
-                <BsSend size={22} className="mt-0.5" />
+            <Link
+                className="link link-hover"
+                href="mailto:hello@johanmontorfano.com"
+            >
+                Contact
             </Link>
         </div>
     </div>
+}
+
+export function Footer() {
+    return <footer className="footer footer-horizontal items-center py-4">
+        <aside className="grid-flow-col items-center">
+            <Image src={Icon} alt="icon" width={20} height={20} />
+            <p className="text-lg">LYONDLE</p>
+        </aside>
+        <nav className="grid-flow-col justify-self-end">
+            <Link className="link link-hover" href="/archive">Archive</Link>
+            <Link
+                className="link link-hover"
+                href="mailto:hello@johanmontorfano.com"
+            >
+                Contact
+            </Link>
+        </nav>
+    </footer>
 }
