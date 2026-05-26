798a0bf121407f99aab50c7c5ccb081b96886c1a
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-26 16:28:38 +0200

    add: fortune, fix: layout for cleaner links list

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,18 +4,40 @@ import Icon from "@/public/icon.svg";
 import { BsEnvelope } from "react-icons/bs";
 
 export function Navbar() {
-    return <div className="w-full flex items-center justify-between">
-        <Image src={Icon} alt="icon" width={25} height={25} />
-        <nav className="flex gap-2 [&>a]:tracking-wide [&>a]:text-sm">
-            <Link className="link link-hover" href="/">Lyondle</Link>
-            <Link className="link link-hover" href="/doku">Doku</Link>
-            <Link className="link link-hover" href="/archive">Archive</Link>
-        </nav>
-        <Link
-            className="link link-hover"
-            href="mailto:hello@johanmontorfano.com"
-        >
-            <BsEnvelope size={20} />
-        </Link>
-    </div>
+    return (
+        <div className="w-full flex items-center justify-between">
+            <Image src={Icon} alt="icon" width={25} height={25} />
+            <nav className="flex gap-2 [&>a]:tracking-wide [&>a]:text-sm">
+                <details className="dropdown">
+                    <summary className="btn btn-ghost">Jeux</summary>
+                    <ul className="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li>
+                            <Link className="link link-hover" href="/">
+                                Lyondle
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="link link-hover" href="/doku">
+                                Doku
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="link link-hover" href="/fortune">
+                                Fortune
+                            </Link>
+                        </li>
+                    </ul>
+                </details>
+                <Link className="btn btn-ghost" href="/archive">
+                    Archive
+                </Link>
+            </nav>
+            <Link
+                className="link link-hover"
+                href="mailto:hello@johanmontorfano.com"
+            >
+                <BsEnvelope size={20} />
+            </Link>
+        </div>
+    );
 }
