07799ef01de5906d34dda8704dc25dbe4c24f749
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-29 00:51:10 +0200

    add: new animations and display proposal for wordle

--- components/wordle/row.tsx
+++ components/wordle/row.tsx
@@ -1,23 +1,28 @@
 import { WordleAnswer } from "@/scripts/game_mgr/types";
+import { useEffect, useState } from "react";
 import { BsArrowDown } from "react-icons/bs";
 
 export function WordleRow(props: WordleAnswer) {
-    const isExactMatch = props.distanceWithAnswer === 0;
+    const match = props.distanceWithAnswer === 0;
+    const [animation, setAnimation] = useState([
+        `animate-flash-${props.validLinesOnStation.length > 0 ? "green" : "red"}`,
+        `animate-flash-${props.cityMatch ? "green" : "red"}`,
+        `animate-flash-${match ? "green" : "red"}`,
+    ]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setAnimation(["", "", ""]), 1000);
+        return () => clearTimeout(timer);
+    }, []);
 
     return (
         <tr className="border-b [&>td]:p-3 font-semibold">
-            <td className={isExactMatch ?
-                "text-success-content bg-success" :
-                "text-error-content bg-error"
-            }>
+            <td>
                 <p className="text-base mb-1 text-dyn-md">
                     {props.guess.name}
                 </p>
             </td>
-            <td className={
-                props.validLinesOnStation.length > 0 ?
-                    "bg-success" : "bg-error"
-            }>
+            <td className={animation[0]}>
                 <div className="flex flex-wrap gap-1 justify-center">
                     {props.validLinesOnStation.sort().map((c) => (
                         <img
@@ -29,10 +34,8 @@ export function WordleRow(props: WordleAnswer) {
                     ))}
                 </div>
             </td>
-            <td className={`p-2 text-center text-dyn-md ${
-                props.cityMatch ?
-                    "text-success-content bg-success" :
-                    "text-error-content bg-error"
+            <td className={`p-2 text-center text-dyn-md ${animation[1]} ${
+                props.cityMatch ? "text-success-content" : "text-error-content"
             }`}>
                 {props.guess.stationLocation} {
                     props.guess.stationBorough > -1 ?
@@ -40,10 +43,9 @@ export function WordleRow(props: WordleAnswer) {
                         ""
                 }
             </td>
-            <td className={isExactMatch ?
-                "text-success-content bg-success" :
-                "text-error-content bg-error"
-            }>
+            <td className={`${animation[2]} ${
+                match ? "text-success-content" : "text-error-content"
+            }`}>
                 <div className="flex items-center justify-end gap-1.5">
                     <span className="text-dyn-md whitespace-nowrap">
                         {props.distanceWithAnswer === 0 ? "Exact" : `${
