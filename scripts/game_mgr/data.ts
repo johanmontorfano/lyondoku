7b2102f0d2a097ff0c0af4401357c4a244673124
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-29 13:19:15 +0200

    add: genesis

--- scripts/game_mgr/data.ts
+++ scripts/game_mgr/data.ts
@@ -1,3 +1,3 @@
-export const firstEverGrid = "05/12/2026";
-export const firstEverGuess = "05/27/2026";
-export const firstEverWordle = "05/18/2026";
+export const firstEverGrid = "05/29/2026";
+export const firstEverGuess = "05/29/2026";
+export const firstEverWordle = "05/29/2026";
