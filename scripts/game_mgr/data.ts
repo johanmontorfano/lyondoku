73971212a87caf42e2f0b76df5a29f262cfeb534
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-06-01 12:13:10 +0200

    fix: wiped test games

--- scripts/game_mgr/data.ts
+++ scripts/game_mgr/data.ts
@@ -1,3 +1,3 @@
-export const firstEverGrid = "01/01/2026";
-export const firstEverWordle = "01/01/2026";
-export const firstEverGuessr = "01/01/2026";
+export const firstEverGrid = "06/01/2026";
+export const firstEverWordle = "06/01/2026";
+export const firstEverGuessr = "06/01/2026";
