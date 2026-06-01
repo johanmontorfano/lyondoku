3c0154f265bb9008e24ba4b94ab52363cbbee6b3
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-06-01 03:14:08 +0200

    add: SEO optimization 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,48 @@ import { ResetProgressShortcut } from "@/components/reset_progress";
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "Lyondle",
-    description: "Connaissez-vous vraiment les TCL ?",
+    title: "Lyondle | Devinez de nouvelles stations TCL chaque jour",
+    description: "Connaissez-vous vraiment les TCL ? Testez-vous chaque jour sur notre Wordle, Doku, et Guessr des stations de métro, tram, funiculaire et navigône de Lyon !",
+    icons: {
+        icon: [
+            {
+                url: "https://www.lyondle.fr/icon.svg",
+                type: "image/svg+xml"
+            },
+            {
+                url: "https://www.lyondle.fr/favicon.ico",
+                sizes: "any"
+            }
+        ],
+        apple: "https://www.lyondle.fr/apple-touch.png"
+    },
+    openGraph: {
+        title: "Lyondle | Devinez de nouvelles stations TCL chaque jour",
+        description:
+            "Testez-vous chaque jour sur un Sudoku, un Wordle et un Guessr 100% dédiés aux TCL.",
+        url: "https://www.lyondle.fr",
+        siteName: "Lyondle",
+        locale: "fr_FR",
+        type: "website",
+        images: [
+            {
+                url: "https://www.lyondle.fr/og-banner.png",
+                width: 1200,
+                height: 630,
+                alt: "Jouer à la trilogie de jeux de Lyondle"
+            }
+        ]
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Lyondle | La trilogie de jeux des TCL",
+        description:
+            "Testez-vous chaque jour sur un Sudoku, un Wordle et un Guessr 100% dédiés aux TCL.",
+        images: ["https://www.lyondle.fr/og-banner.png"]
+    },
+    alternates: {
+        canonical: "https://www.lyondle.fr"
+    }
 };
 
 export default function RootLayout({
