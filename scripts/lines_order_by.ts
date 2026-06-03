c0b450fa22e3f31dd759c2c095ab34af9c0b3c80
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-06-03 17:02:31 +0200

    fix: ordering

--- scripts/lines_order_by.ts
+++ scripts/lines_order_by.ts
@@ -1,9 +1,9 @@
 export function orderLines(lines: string[]) {
     return lines.sort((a, b) => {
         if (a.startsWith("M")) return -1;
-        if (b.startsWith("M")) return 1;
-        if (a.startsWith("T")) return -1;
-        if (b.startsWith("T")) return 1;
+        if (b.startsWith("M")) return 3;
+        if (a.startsWith("T")) return -3;
+        if (b.startsWith("T")) return 2;
         if (a.startsWith("F")) return -1;
         if (b.startsWith("F")) return 1;
         return 0;
